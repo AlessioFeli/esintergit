f409fcc655414c3a936fdd5a388a140d505cd031
AlessioFeli <alefelino03@gmail.com>
2021-12-13 16:57:05 +0100

    ciao mondo

---
+++ app/src/main/java/com/example/esinter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.esinter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.esinter.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    String nazioni[] = {"italia", "francia", "spagna", "germania"};
+    ListView listastati;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnesegui = (Button) findViewById(R.id.btnEsegui);
+
+        btnesegui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        listastati = (ListView) findViewById(R.id.listastati);
+        ArrayAdapter<String> astati = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nazioni);
+        listastati.setAdapter(astati);
+    }
+}
